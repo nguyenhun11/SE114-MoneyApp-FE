f65a3db81461880488a126d41c8223a51fab9e2a
nguyenhun11 <giahungcantho11@gmail.com>
2026-04-17 19:20:44 +0700

    fix: transaction

---
+++ app/src/main/java/com/example/moneyapp/data/model/CategorySummary.java
@@ -0,0 +1,8 @@
+package com.example.moneyapp.data.model;
+
+public class CategorySummary {
+    public String name;
+    public String icon;
+    public String color;
+    public double amount;
+}
---
+++ app/src/main/java/com/example/moneyapp/data/repository/TransactionRepository.java
@@ -0,0 +1,82 @@
+package com.example.moneyapp.data.repository;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.example.moneyapp.data.local.AppDatabase;
+import com.example.moneyapp.data.local.dao.TransactionDao;
+import com.example.moneyapp.data.local.entity.Transaction;
+import com.example.moneyapp.utils.PreferenceManager;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TransactionRepository {
+    private final TransactionDao transactionDao;
+    private final ExecutorService executorService;
+    private final Context context;
+    private String currentUserID;
+
+    public interface TransactionCallback{
+        void onSuccess(Transaction transaction);
+        void onSuccess(List<Transaction> transactionList);
+        void onError(String message);
+    }
+
+    public TransactionRepository(Application application) {
+        AppDatabase appDatabase = AppDatabase.getInstance(application);
+        this.transactionDao = appDatabase.transactionDao();
+        this.executorService = Executors.newSingleThreadExecutor();
+        this.context = application.getApplicationContext();
+        this.currentUserID = PreferenceManager.getInstance(context).getUserID();
+    }
+
+    public void addTransaction(Transaction transaction, TransactionCallback callback){
+        executorService.execute(()->{
+            try {
+                transactionDao.insertTransaction(transaction);
+                callback.onSuccess(transaction);
+            }
+            catch (Exception e){
+                callback.onError("System error: " + e.getMessage());
+            }
+        });
+    }
+
+    public void getTransactionByID(String transactionID, TransactionCallback callback){
+        executorService.execute(()-> {
+            try{
+                Transaction transaction = transactionDao.getTransactionById(transactionID);
+                callback.onSuccess(transaction);
+            } catch (Exception e) {
+                callback.onError("System error" + e.getMessage());
+            }
+        });
+    }
+
+    public void getFilteredTransactions(
+            int type,
+            Date startDate,
+            Date endDate,
+            String accountID,  //null if all
+            String categoryID, //null if all
+            TransactionCallback callback){
+        executorService.execute(()->{
+           try {
+               List<Transaction> transactionList = transactionDao.getAllTransactions(
+                       type,
+                       startDate,
+                       endDate,
+                       accountID,
+                       categoryID,
+                       currentUserID
+               );
+               callback.onSuccess(transactionList);
+           } catch (Exception e) {
+               callback.onError("System error" + e.getMessage());
+           }
+        });
+    }
+}
