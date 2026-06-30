c5c75738afe51fc45db4f91f104caa7d2f92ed8c
nguyenhun11 <giahungcantho11@gmail.com>
2026-06-30 11:20:34 +0700

    remove unnecessary file

--- app/src/main/java/com/example/moneyapp/data/remote/api/RetrofitClient.java
+++ app/src/main/java/com/example/moneyapp/data/remote/api/RetrofitClient.java
@@ -16,9 +16,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    public static final String BASE_URL = "http://192.168.1.9:5005/";
+    //public static final String BASE_URL = "http://192.168.1.9:5005/";
     //public static final String BASE_URL = "http://10.0.2.2:5005/";
-    //public static final String BASE_URL = "http://nguyenhun11-001-site1.site4future.com/";
+    public static final String BASE_URL = "http://nguyenhun11-001-site1.site4future.com/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getInstance(Context context) {
